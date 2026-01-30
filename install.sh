87df60dae00bd1db71f5f14a18324f1cbf1eef21
rizkyyp12 <100074964+rizkyyp12@users.noreply.github.com>
2026-01-30 23:07:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo " OpenClash Auto DIRECT + Huawei Recovery"
 echo " INSTALLER"
 echo "========================================"
 
-REPO_RAW="https://raw.githubusercontent.com/rizkyyp12/huawei-curl-auto-ava/main.py"
+REPO_RAW="https://raw.githubusercontent.com/rizkyyp12/huawei-curl/main.py"
 
 BIN_DIR="/usr/bin"
 INIT_DIR="/etc/init.d"
